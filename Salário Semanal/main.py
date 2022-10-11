ece1c8d4cba7d4e2b8a78f25a7128b2230550f2d
Diogo Vidal <111745761+diogovidall@users.noreply.github.com>
2022-10-11 09:00:53 -0300

    Add files via upload

---
+++ Salário Semanal/main.py
@@ -0,0 +1,10 @@
+print('Salário Semanal')
+seg = int(input('Segunda Feira:'))
+ter = int(input('Terça Feira:'))
+qua = int(input('Quarta Feira:'))
+qui = int(input('Quinta Feira:'))
+sex = int(input('Sexta Feira:'))
+sab = int(input('Sábado:'))
+
+result = seg+ter+qua+qui+sex+sab
+print(f'Resultado da Semana: {result}')
